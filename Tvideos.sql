12b7e52d947790cce11c9e7560c778331c3bac30
schertzer dylan <dylan.schertzer@outlook.fr>
2023-02-02 11:54:50 +0300

    final final commit

--- Tvideos.sql
+++ Tvideos.sql
@@ -1,38 +1,39 @@
-CREATE TABLE Video (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  titre TEXT ,
-  lien TEXT ,
-  description TEXT ,
-  like INTEGER ,
-  dislike INTEGER ,
-  id_chaine INTEGER ,
-  vue INTEGER,
-  timestamp INTEGER ,
-  FOREIGN KEY (id_chaine) REFERENCES Chaine(id)
-);
+CREATE TABLE IF NOT EXISTS videos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        titre TEXT,
+        lien TEXT,
+        description TEXT,
+        like INTEGER,
+        dislike INTEGER,
+        id_chaine INTEGER,
+        vue INTEGER,
+        timestamp INTEGER,
+        FOREIGN KEY (id_chaine) REFERENCES chaines(id)
+        );
+CREATE TABLE IF NOT EXISTS chaines (
+        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        pseudo TEXT,
+        abonnes INTEGER,
+        bio TEXT,
+        timestamp INTEGER,
+        email TEXT
+        );
 
-CREATE TABLE Chaine (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  pseudo TEXT,
-  abonnes INTEGER,
-  bio TEXT,
-  timestamp INTEGER ,
-  email TEXT 
-);
+CREATE TABLE IF NOT EXISTS commentaires (
+        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        contenue TEXT,
+        like INTEGER,
+        id_chaine INTEGER,
+        id_video INTEGER,
+        timestamp INTEGER,
+        FOREIGN KEY (id_chaine) REFERENCES chaines(id),
+        FOREIGN KEY (id_video) REFERENCES videos(id)
+        );
 
-CREATE TABLE Commentaire (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  contenue TEXT,
-  like INTEGER,
-  id_chaine TEXT,
-  timestamp INTEGER,
-  FOREIGN KEY (id_chaine) REFERENCES Chaine(id)
-);
-
-CREATE TABLE n_com (
-  id_video INTEGER,
-  id_commentaire INTEGER,
-  PRIMARY KEY (id_video, id_commentaire),
-  FOREIGN KEY (id_video) REFERENCES Video(id),
-  FOREIGN KEY (id_commentaire) REFERENCES Commentaire(id)
-);
+CREATE TABLE IF NOT EXISTS n_com ( 
+        id_video INTEGER,
+        id_commentaire INTEGER,
+        PRIMARY KEY (id_video, id_commentaire),
+        FOREIGN KEY (id_video) REFERENCES Video(id),
+        FOREIGN KEY (id_commentaire) REFERENCES Commentaire(id)
+        );
